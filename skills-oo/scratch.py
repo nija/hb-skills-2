a0652bdd9864f2c195cd3b95c6eb6cb3f502b56f
Nija Mashruwala <nija.mashruwala@gmail.com>
2016-04-24 15:15:04 -0700

    Added Quiz class

--- skills-oo/scratch.py
+++ skills-oo/scratch.py
@@ -1,11 +1,13 @@
 
 
 exam = Exam('Midterm')
-exam.add_question('What is the method for adding an element to a set?','.add()')
-exam.add_question('What is the capital of Alberta?','Edmonton')
-exam.add_question('Who is the author of Python?','Guido Van Rossum')
-exam.add_question("Who is Ubermelon's competition?",'Sqysh')
-exam.add_question("What is Balloonicorn's favorite color?",'Sparkles')
+exam.add_question('What is the method for adding an element to a set?', '.add()')
+exam.add_question('What is the capital of Alberta?', 'Edmonton')
+exam.add_question('Who is the author of Python?', 'Guido Van Rossum')
+exam.add_question("Who is Ubermelon's competition?", 'Sqysh')
+exam.add_question("What is Balloonicorn's favorite color?", 'Sparkles')
+print exam.questions[0]
+
 
 for q in exam.questions:
     print q.question
@@ -13,3 +15,13 @@ for q in exam.questions:
     print exam.score
 
 exam.administer()
+
+quiz = Quiz('Quiz 1')
+
+quiz.add_question('What is the method for adding an element to a set?', '.add()')
+quiz.add_question('What is the capital of Alberta?', 'Edmonton')
+quiz.add_question('Who is the author of Python?', 'Guido Van Rossum')
+quiz.add_question("Who is Ubermelon's competition?", 'Sqysh')
+quiz.add_question("What is Balloonicorn's favorite color?", 'Sparkles')
+
+quiz.administer()
--- skills-oo/skills.py
+++ skills-oo/skills.py
@@ -10,30 +10,34 @@
 # Explain each concept.
 
 # 1. What is a class?
-# It's a generalized definition of a single concept. 
+# It's a generalized definition of a single concept.
 
 # 2. What is an instance attribute?
-# It's an attribute specific to an instance of an object. 
+# It's an attribute specific to an instance of an object.
 
 # 3. What is a method?
 # It's a function that's tied to a class.
 
 # 4. What is an instance in object orientation?
-# It's a specific, actual, individual object. 
+# It's a specific, actual, individual object.
 
-# 5. How is a class attribute different than an instance attribute? Give an example of when you might use each.
+# 5. How is a class attribute different than an instance attribute? Give an example of when you
+# might use each.
 # A class attribute is one attribute shared for the whole class, whereas the instance attribute is
-# specific to that instance. 
+# specific to that instance.
 
 
 # Parts 2 through 5:
 # Create your classes and class methods
 
-# Directions: Make Python classes that could store the each of the following three pieces of data. Use the dictionaries below as examples to guide you in creating class definitions for the following objects. Define an __init__ method to allow callers of this class to provide initial values for each attribute.
+# Directions: Make Python classes that could store the each of the following three pieces of data.
+# Use the dictionaries below as examples to guide you in creating class definitions for the
+# following objects. Define an __init__ method to allow callers of this class to provide initial
+# values for each attribute.
 
 # Student
 
-# Students can have first and last names and addresses. Here are two example students. Write a 
+# Students can have first and last names and addresses. Here are two example students. Write a
 # class that can store data like this:
 
 # {'first_name': 'Jasmine',
@@ -44,17 +48,18 @@
 #  'last_name': 'Console',
 #  'address': '888 Binary Ave'}
 
-class Student():
+class Student(object):
 
-	def __init__(self, first_name, last_name, address):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.address = address
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
 
 
 # Question
 
-# Questions are a question & a correct answer; here are two example questions. Write a class that could store data like this:
+# Questions are a question & a correct answer; here are two example questions. Write a class that
+# could store data like this:
 
 # {'question': 'What is the capital of Alberta?',
 #  'correct_answer': 'Edmonton'}
@@ -62,8 +67,8 @@ class Student():
 # {'question': 'Who is the author of Python?',
 #  'correct_answer': 'Guido Van Rossum'}
 
-# Add a method to the Question class that prints the question to the console and prompts the user 
-# for an answer. It should return True or False depending on whether the correct answer matches 
+# Add a method to the Question class that prints the question to the console and prompts the user
+# for an answer. It should return True or False depending on whether the correct answer matches
 # the user's answer.
 
 # For example
@@ -76,27 +81,30 @@ class Student():
 # What is the method for adding an element to a set? > .add()
 # True
 
-class Question():
+class Question(object):
 
-	def __init__(self, question, answer):
-		self.question = question
-		self.correct_answer = answer
+    def __init__(self, question, answer):
+        self.question = question
+        self.correct_answer = answer
 
-	def ask_and_evaluate(self):
-		possible_answer = raw_input(self.question + " ")
-		return (possible_answer == self.correct_answer)
+    def __repr__(self):
+        return self.question + " " + self.correct_answer
+
+    def ask_and_evaluate(self):
+        possible_answer = raw_input(self.question + " ")
+        return possible_answer == self.correct_answer
 
 
 # Exam
 
-# Notice that an Exam should have an attribute called questions. Simply initialize the questions 
-# attribute as an empty list in the body __init__ function. We'll deal with adding questions to 
-# the exam later on in this assignment. Your __init__ function should take a name for the exam 
+# Notice that an Exam should have an attribute called questions. Simply initialize the questions
+# attribute as an empty list in the body __init__ function. We'll deal with adding questions to
+# the exam later on in this assignment. Your __init__ function should take a name for the exam
 # as a parameter.
 
 # A Note on Attributes
-# Though we've mainly seen attributes that are strings or integers, remember: attributes can also 
-# be lists, and many other data types. In the case of our questions attribute, we'll have a list 
+# Though we've mainly seen attributes that are strings or integers, remember: attributes can also
+# be lists, and many other data types. In the case of our questions attribute, we'll have a list
 # of Question objects.
 
 # For example, here are two exams. Make a class that could store data like this:
@@ -125,7 +133,7 @@ class Question():
 #   ]
 # }
 
-# Add a method to the Exam class which takes a question and a correct_answer as parameters, makes 
+# Add a method to the Exam class which takes a question and a correct_answer as parameters, makes
 # a Question from those, and adds it to the exam's list of questions.
 
 # For example
@@ -136,7 +144,7 @@ class Question():
 # ...     'What is the method for adding an element to a set?',
 # ...     '.add()')
 
-# Add a method to the Exam class which administers all of the exam's questions, and returns the 
+# Add a method to the Exam class which administers all of the exam's questions, and returns the
 # user's score at the end.
 
 # So, building on our code from problem 2, here's how the Exam class should work.
@@ -153,7 +161,7 @@ class Question():
 # Hint
 # Here's some pseudocode for the administer method.
 
-# Inside the Exam.administer method, you'll need to first initialize a variable called score; 
+# Inside the Exam.administer method, you'll need to first initialize a variable called score;
 # set it to zero.
 
 # Next, loop through each of the questions in the exam.
@@ -165,33 +173,70 @@ class Question():
 # After the last question has been administered, return the score.
 
 
-class Exam():
+class Exam(object):
+    ''' Placeholder
+    '''
 
-	def __init__(self, name, questions = [] ):
-		self.score = 0
-		self.name = name
-		self.questions = questions
+    def __init__(self, name, questions=[]):
+        ''' Placeholder
+        '''
+        self.score = 0
+        self.name = name
+        self.questions = questions
 
-	def add_question(self, question, correct_answer):
-
-		self.questions.append(Question(question, correct_answer))
-
-
-	def administer(self):
-
-		for q in self.questions:
-			if q.ask_and_evaluate():
-				self.score += 1
-
-		return self.score
+    def add_question(self, question, correct_answer):
+        ''' Placeholder
+        '''
+        self.questions.append(Question(question, correct_answer))
 
 
-# Write a function, take_test, that takes an exam and a student as parameters, administers the 
+    def administer(self):
+        ''' Placeholder
+        '''
+        for question in self.questions:
+            if question.ask_and_evaluate():
+                self.score += 1.00
+
+        return (self.score / len(self.questions)) * 100.0
+
+
+
+
+# A "quiz" is like an exam - it's a set of questions that students are prompted to answer. However,
+# whereas exams are given a percentage score, quizzes are pass/fail: if you answered at least half
+# of the questions correct, you pass the quiz. When you call the administer method on a quiz, it
+# should only return True if you passed or False if you failed.
+#
+# Think about we could solve this requirement: we have an Exam class and we want to have a Quiz
+# class that is similar.
+#
+# Write code to solve this problem. Incorporate as many of the "design" parts of the class lectures
+# as you feel comfortable with.
+
+class Quiz(Exam):
+    ''' Placeholder
+    '''
+
+    def administer(self):
+        ''' Placeholder
+        '''
+
+        result = super(Quiz, self).administer()
+        if result > 69.9:
+            print "Pass"
+        else:
+            print "Fail"
+        return result
+
+
+# Write a function, take_test, that takes an exam and a student as parameters, administers the
 # exam, and assigns the score to the student instance as a new attribute called score.
 
 def take_test(exam, student):
-	student.score = exam.administer()
-	print student.score
+    ''' Placeholder
+    '''
+    student.score = exam.administer()
+    print student.score
 
 
 # Write a function, example, which does the following:
@@ -202,27 +247,32 @@ def take_test(exam, student):
 # Administers the test for that student
 
 def example():
-	exam = Exam('Exam PLE')
-	exam.add_question('What is the method for adding an element to a set?','.add()')
-	exam.add_question('What is the capital of Alberta?','Edmonton')
-	exam.add_question('Who is the author of Python?','Guido Van Rossum')
-	exam.add_question("Who is Ubermelon's competition?",'Sqysh')
-	exam.add_question("What is Balloonicorn's favorite color?",'Sparkles')
+    ''' Placeholder
+    '''
+    exam = Exam('Exam PLE')
+    exam.add_question('What is the method for adding an element to a set?', '.add()')
+    exam.add_question('What is the capital of Alberta?', 'Edmonton')
+    exam.add_question('Who is the author of Python?', 'Guido Van Rossum')
+    exam.add_question("Who is Ubermelon's competition?", 'Sqysh')
+    exam.add_question("What is Balloonicorn's favorite color?", 'Sparkles')
 
-	chriki_chekorir = Student('Chriki', 'Chekorir', 'A village in Kenya')
+    chriki_chekorir = Student('Chriki', 'Chekorir', 'A village in Kenya')
 
-	take_test(exam, chriki_chekorir)
+    take_test(exam, chriki_chekorir)
 
 
-# A “quiz” is like an exam — it’s a set of questions that students are prompted to answer. However, 
-# whereas exams are given a percentage score, quizzes are pass/fail: if you answered at least half 
-# of the questions correct, you pass the quiz. When you call the administer method on a quiz, it 
-# should only return True if you passed or False if you failed.
-#
-# Think about we could solve this requirement: we have an Exam class and we want to have a Quiz
-# class that is similar.
-#
-# Write code to solve this problem. Incorporate as many of the “design” parts of the class lectures
-# as you feel comfortable with.
+def quiz_example():
+    ''' pass
+    '''
+    quiz = Quiz('Quiz ZLE')
+    quiz.add_question('What is the method for adding an element to a set?', '.add()')
+    quiz.add_question('What is the capital of Alberta?', 'Edmonton')
+    quiz.add_question('Who is the author of Python?', 'Guido Van Rossum')
+    quiz.add_question("Who is Ubermelon's competition?", 'Sqysh')
+    quiz.add_question("What is Balloonicorn's favorite color?", 'Sparkles')
 
-# I ran out of time for this :-(
+    chriki_chekorir = Student('Chriki', 'Chekorir', 'A village in Kenya')
+
+    take_test(quiz, chriki_chekorir)
+
+
